2a48809d63fd5c711b82103bfc469bcf86ac6510
Jakub Bilko <jakub.bilko@10clouds.com>
2024-01-23 18:20:58 +0800

    feat: settings form wip

--- frontend/src/components/settings/modal/GlobalSettingsModal.tsx
+++ frontend/src/components/settings/modal/GlobalSettingsModal.tsx
@@ -14,9 +14,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { X } from 'lucide-react';
 import { Content, Portal, Root } from '@radix-ui/react-dialog';
+import { useForm } from 'react-hook-form';
 
 import TopGradient from '@/components/common/TopGradient';
 import { useSettingsStore } from '@/store/settings/useSettingsStore';
@@ -25,91 +26,109 @@ import { Icon } from '../../common/icons/Icon';
 import GlobalSettingsApiSection from './sections/GlobalSettingsApiSection';
 import GlobalSettingsCodeSection from './sections/GlobalSettingsCodeSection';
 import GlobalSettingsUserSection from './sections/GlobalSettingsUserSection';
+import { GlobalSettingsFormData } from '@/forms/globalSettingsForm';
 
 // TODO: implement other features from figma like api for azure, user profile and tutorial
 export const GlobalSettingsModal = () => {
   const isSettingsModalVisible = useSettingsStore((state) => state.isSettingsModalVisible);
   const setSettingsModalVisibility = useSettingsStore((state) => state.setSettingsModalVisibility);
+
+  const username = useSettingsStore((state) => state.username);
+  const email = useSettingsStore((state) => state.userEmail);
+  const userAvatarUrl = useSettingsStore((state) => state.userAvatarUrl);
   const openAiApiKey = useSettingsStore((state) => state.openAiApiKey);
 
-  const [usernameFormValue, setUsernameFormValue] = useState<string | undefined>(undefined);
-  const username = useSettingsStore((state) => state.username);
-  useEffect(() => {
-    setUsernameFormValue(username);
-  }, [username]);
-
-  const [emailFormValue, setEmailFormValue] = useState<string | undefined>(undefined);
-  const email = useSettingsStore((state) => state.userEmail);
-  useEffect(() => {
-    setEmailFormValue(email);
-  }, [email]);
-
-  const [apiKeyValue, setApiKeyValue] = useState<string | undefined>(openAiApiKey);
+  const { reset, control, setValue } = useForm<GlobalSettingsFormData>();
 
   useEffect(() => {
-    setApiKeyValue(openAiApiKey || '');
-  }, [openAiApiKey]);
-
-  const [isAutoRun, setIsAutoRun] = useState(false);
-  const alwaysExecuteCode = useSettingsStore((state) => state.alwaysExecuteCode);
-  useEffect(() => {
-    setIsAutoRun(alwaysExecuteCode);
-  }, [alwaysExecuteCode]);
-
-  const [userAvatarData, setUserAvatarData] = useState<File>();
-  const [isAvatarOverwritten, setIsAvatarOverwritten] = useState(false);
-
-  const handleAutoRunChange = (autorun: boolean) => {
-    setIsAutoRun(autorun);
-  };
-
-  const save = async () => {
-    let avatarFormData: FormData | null = null;
-
-    // check if avatar was overwritten to avoid sending unnecessary requests
-    if (isAvatarOverwritten && userAvatarData) {
-      avatarFormData = new FormData();
-      avatarFormData.append('avatar', userAvatarData);
-    }
-
-    useSettingsStore.getState().saveSettings(
-      {
-        user_profile:
-          usernameFormValue !== username || emailFormValue !== email
-            ? {
-                username: usernameFormValue !== username ? usernameFormValue : undefined,
-                email: emailFormValue !== email ? emailFormValue : undefined,
-              }
-            : undefined,
-        openai_api_key: apiKeyValue !== openAiApiKey ? apiKeyValue : undefined,
-        code_autorun: isAutoRun !== alwaysExecuteCode ? isAutoRun : undefined,
-      },
-      true,
-      avatarFormData,
-    );
-    setSettingsModalVisibility(false);
-  };
-
-  const handleSetAvatarImage = (avatar: File) => {
-    setUserAvatarData(avatar);
-    setIsAvatarOverwritten(true);
-  };
-
-  useEffect(() => {
-    const resetState = () => {
-      setUsernameFormValue(username);
-      setEmailFormValue(email);
-      setApiKeyValue(openAiApiKey);
-      setIsAutoRun(alwaysExecuteCode);
-      setUserAvatarData(undefined);
-      setIsAvatarOverwritten(false);
-    };
-
     if (isSettingsModalVisible) {
-      resetState();
+      reset({
+        user_profile: {
+          username,
+          email: email || '',
+        },
+        openai_api_key: openAiApiKey,
+        avatarUrl: userAvatarUrl,
+      });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isSettingsModalVisible]); // reset state when modal is closed or opened
+  }, [isSettingsModalVisible]);
+
+  // const [usernameFormValue, setUsernameFormValue] = useState<string | undefined>(undefined);
+  //
+  // useEffect(() => {
+  //   setUsernameFormValue(username);
+  // }, [username]);
+
+  // const [emailFormValue, setEmailFormValue] = useState<string | undefined>(undefined);
+  //
+  // useEffect(() => {
+  //   setEmailFormValue(email);
+  // }, [email]);
+
+  // const [apiKeyValue, setApiKeyValue] = useState<string | undefined>(openAiApiKey);
+
+  // useEffect(() => {
+  //   setApiKeyValue(openAiApiKey || '');
+  // }, [openAiApiKey]);
+
+  // const [isAutoRun, setIsAutoRun] = useState(false);
+  // const alwaysExecuteCode = useSettingsStore((state) => state.alwaysExecuteCode);
+  // useEffect(() => {
+  //   setIsAutoRun(alwaysExecuteCode);
+  // }, [alwaysExecuteCode]);
+
+  // const [userAvatarData, setUserAvatarData] = useState<File>();
+  // const [isAvatarOverwritten, setIsAvatarOverwritten] = useState(false);
+
+  // const handleAutoRunChange = (autorun: boolean) => {
+  //   setIsAutoRun(autorun);
+  // };
+
+  // const save = async () => {
+  //   let avatarFormData: FormData | null = null;
+
+  //   // check if avatar was overwritten to avoid sending unnecessary requests
+  //   if (isAvatarOverwritten && userAvatarData) {
+  //     avatarFormData = new FormData();
+  //     avatarFormData.append('avatar', userAvatarData);
+  //   }
+
+  //   useSettingsStore.getState().saveSettings(
+  //     {
+  //       user_profile:
+  //         usernameFormValue !== username || emailFormValue !== email
+  //           ? {
+  //               username: usernameFormValue !== username ? usernameFormValue : undefined,
+  //               email: emailFormValue !== email ? emailFormValue : undefined,
+  //             }
+  //           : undefined,
+  //       openai_api_key: apiKeyValue !== openAiApiKey ? apiKeyValue : undefined,
+  //       code_autorun: isAutoRun !== alwaysExecuteCode ? isAutoRun : undefined,
+  //     },
+  //     true,
+  //     avatarFormData,
+  //   );
+  //   setSettingsModalVisibility(false);
+  // };
+
+  const handleSetAvatarImage = (avatar: File) => setValue('avatar', avatar);
+
+  // useEffect(() => {
+  //   const resetState = () => {
+  //     setUsernameFormValue(username);
+  //     setEmailFormValue(email);
+  //     setApiKeyValue(openAiApiKey);
+  //     setIsAutoRun(alwaysExecuteCode);
+  //     setUserAvatarData(undefined);
+  //     setIsAvatarOverwritten(false);
+  //   };
+
+  //   if (isSettingsModalVisible) {
+  //     resetState();
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [isSettingsModalVisible]); // reset state when modal is closed or opened
 
   const handleModalClose = () => {
     setSettingsModalVisibility(false);
@@ -132,19 +151,18 @@ export const GlobalSettingsModal = () => {
 
             <div className="h-[calc(100%-100px)] max-w-[720px] mx-auto relative flex flex-col justify-center gap-5">
               <GlobalSettingsUserSection
-                email={emailFormValue}
-                setEmail={setEmailFormValue}
-                username={usernameFormValue}
-                setUsername={setUsernameFormValue}
-                setImage={handleSetAvatarImage}
+                control={control}
+                avatarUrl={userAvatarUrl}
+                onImageSelected={handleSetAvatarImage}
               />
-              <GlobalSettingsApiSection apiKey={apiKeyValue} setApiKey={setApiKeyValue} />
+              {/* <GlobalSettingsApiSection apiKey={apiKeyValue} setApiKey={setApiKeyValue} />
               <GlobalSettingsCodeSection isAutoRun={isAutoRun} setIsAutoRun={handleAutoRunChange} />
+               */}
               <div className="flex items-center justify-end gap-[10px] py-[40px]">
                 <Button variant="secondary" bold onClick={handleModalClose}>
                   Cancel
                 </Button>
-                <Button onClick={save}>{'Save'}</Button>
+                <Button>{'Save'}</Button>
               </div>
             </div>
           </div>
--- frontend/src/components/settings/modal/sections/GlobalSettingsUserSection.tsx
+++ frontend/src/components/settings/modal/sections/GlobalSettingsUserSection.tsx
@@ -14,92 +14,65 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { useState } from 'react';
+import { Controller, useWatch } from 'react-hook-form';
+import type { Control } from 'react-hook-form';
+import { Pencil } from 'lucide-react';
+
 import ImageUploader from '@/components/common/ImageUploader';
 import { Icon } from '@/components/common/icons/Icon';
 import { TextInput } from '@/components/editables/assets/TextInput';
-import { useSettingsStore } from '@/store/settings/useSettingsStore';
-import { Pencil } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { GlobalSettingsFormData } from '@/forms/globalSettingsForm';
 
 interface GlobalSettingsUserSectionProps {
-  username?: string;
-  email?: string;
-  setUsername: (value: string) => void;
-  setEmail: (value: string) => void;
-  setImage: (avatar: File) => void;
+  control: Control<GlobalSettingsFormData>;
+  onImageSelected: (avatar: File) => void;
+  avatarUrl?: string;
 }
 
-const GlobalSettingsUserSection = ({
-  email,
-  username,
-  setEmail,
-  setUsername,
-  setImage,
-}: GlobalSettingsUserSectionProps) => {
-  const currentUsername = useSettingsStore((state) => state.username) || '';
-  const currentEmail = useSettingsStore((state) => state.userEmail) || '';
-  const userAvatarUrl = useSettingsStore((state) => state.userAvatarUrl) || undefined;
-
+const GlobalSettingsUserSection = ({ onImageSelected, avatarUrl, control }: GlobalSettingsUserSectionProps) => {
   const [isEditMode, setIsEditMode] = useState(false);
-  const [usernameFormValue, setUsernameFormValue] = useState(username);
-  const [emailFormValue, setEmailFormValue] = useState(email);
 
-  useEffect(() => {
-    if (typeof email === 'string') {
-      setEmailFormValue(email);
-    } else {
-      setEmailFormValue(currentEmail);
-    }
-  }, [email, currentEmail]);
+  const watchName = useWatch({ control, name: 'user_profile.username' });
 
-  useEffect(() => {
-    if (typeof username === 'string') {
-      setUsernameFormValue(username);
-    } else {
-      setUsernameFormValue(currentUsername);
-    }
-  }, [username, currentUsername]);
-
-  const handleNameInputBlur = () => {
-    if (!username) {
-      setUsername(currentUsername);
-    }
-    setIsEditMode(false);
-  };
+  const handleNameInputBlur = () => setIsEditMode(false);
 
   return (
     <div className="flex items-center w-full gap-[30px]">
-      <ImageUploader currentImage={userAvatarUrl} onUpload={setImage} />
+      <ImageUploader currentImage={avatarUrl} onUpload={onImageSelected} />
       <div className="flex flex-col justify-between h-full">
         <div className="flex gap-2.5 text-[25px] font-black pt-[30px]">
           <h2 className="text-gray-400">Hello, </h2>
           {isEditMode ? (
-            <input
-              type="text"
-              value={usernameFormValue}
-              onChange={(e) => setUsername(e.target.value)}
-              className="bg-transparent text-white rounded-[8px] outline-none focus:ring-1 focus:ring-gray-400 transition duration-100"
-              onBlur={handleNameInputBlur}
-              autoFocus
+            <Controller
+              name="user_profile.username"
+              control={control}
+              render={({ field }) => (
+                <input
+                  type="text"
+                  className="bg-transparent text-white rounded-[8px] outline-none focus:ring-1 focus:ring-gray-400 transition duration-100"
+                  autoFocus
+                  {...field}
+                  onBlur={handleNameInputBlur}
+                />
+              )}
             />
           ) : (
             <div className="flex gap-5 items-center">
-              <h2 className="text-white">{username || currentUsername}</h2>
+              <h2 className="text-white">{watchName}</h2>
               <button onClick={() => setIsEditMode(true)}>
                 <Icon icon={Pencil} className="text-gray-400 h-6 w-6" />
               </button>
             </div>
           )}
-        </div>
-        <div className="flex flex-col gap-2.5 w-[255px]">
-          <p className="text-white text-[15px]">E-mail address</p>
-          <TextInput
-            value={emailFormValue || ''}
-            name="email"
-            onChange={setEmail}
-            type="email"
-            placeholder="Write e-mail here"
-          />
+          <div className="flex flex-col gap-2.5 w-[255px]">
+            <p className="text-white text-[15px]">E-mail address</p>
+            <Controller
+              name="user_profile.email"
+              control={control}
+              render={({ field }) => <TextInput type="email" placeholder="Write e-mail here" {...field} />}
+            />
+          </div>
         </div>
       </div>
     </div>
