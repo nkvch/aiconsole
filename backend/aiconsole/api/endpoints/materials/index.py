05a6c4dcf21214c0811977b6d6351a321f75aad1
bgozlinski <BARTLOMIEJ.GOZLINSKI@GMAIL.COM>
2024-08-27 03:49:36 +0200

    changed fetch_materials endpoint

--- backend/aiconsole/api/endpoints/materials/index.py
+++ backend/aiconsole/api/endpoints/materials/index.py
@@ -13,8 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
 
 from aiconsole.core.assets.types import AssetType
@@ -23,21 +24,23 @@ from aiconsole.core.project import project
 from aiconsole.database import get_db
 from aiconsole.models.material import Material
 from aiconsole.schemas.material import MaterialResponse
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 
-@router.get("/", response_model=list[MaterialResponse])
+@router.get("/", response_model=List[MaterialResponse])
 async def fetch_materials(db: Session = Depends(get_db)):
-    materials = db.query(Material).all()
+    try:
+        materials = db.query(Material).all()
 
-    response = [
-        {
-            **material.__dict__,
-            "status": project.get_project_agents().get_status(AssetType.MATERIAL, material.id),
-        }
-        for material in materials
-    ]
+        materials_list = [MaterialResponse.from_orm(material) for material in materials]
 
-    return JSONResponse(content=response)
+        return materials_list
+
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=f'{e}')
+
+    finally:
+        db.close()
